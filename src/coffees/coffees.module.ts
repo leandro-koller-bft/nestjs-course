7ab15e74287dcfd22dad6c99044641af85caa11b
Leandro Koller <leandro@brasilfone.com.br>
2022-07-07 18:06:14 -0300

    End of the second day

--- src/coffees/coffees.module.ts
+++ src/coffees/coffees.module.ts
@@ -1,14 +1,19 @@
-import { Module } from '@nestjs/common';
+import { Inject, Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { CoffeesController } from './coffees.controller';
 import { CoffeesService } from './coffees.service';
 import { Coffee } from './entities/coffee.entity';
 import { Flavor } from './entities/flavor.entity';
 import { Event } from 'src/events/entities/event.entity';
+import { COFFEE_BRANDS } from './coffees.contants';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Coffee, Flavor, Event])],
   controllers: [CoffeesController],
-  providers: [CoffeesService],
+  providers: [
+    CoffeesService,
+    { provide: COFFEE_BRANDS, useValue: ['buddy brew', 'nescafe'] },
+  ],
+  exports: [CoffeesService],
 })
 export class CoffeesModule {}
--- typeorm-cli.config.ts
+++ typeorm-cli.config.ts
@@ -1,6 +1,5 @@
 import { Coffee } from "src/coffees/entities/coffee.entity";
 import { Flavor } from "src/coffees/entities/flavor.entity";
-import { CoffeeRefactor1657221258745 } from "src/migrations/1657221258745-CoffeeRefactor";
 import { SchemaSync1657223507222 } from "src/migrations/1657223507222-SchemaSync";
 import { DataSource } from "typeorm";
 
@@ -12,5 +11,5 @@ export default new DataSource({
   password: 'postgres',
   database: 'postgres',
   entities: [Coffee, Flavor],
-  migrations: [CoffeeRefactor1657221258745, SchemaSync1657223507222], 
+  migrations: [SchemaSync1657223507222], 
 })
